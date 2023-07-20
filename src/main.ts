29f68b14fa05223871a2d1aa6cf18578e66949d9
Johnson Fash <fashanutosin7@gmail.com>
2023-07-20 12:33:57 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { HttpExceptionFilter, TransformInterceptor } from './common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor())
