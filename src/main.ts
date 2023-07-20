b518fc0b0013dcef4dd717f3344cf6b61f78f2d6
Johnson Fash <fashanutosin7@gmail.com>
2023-07-20 13:19:33 +0100

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { HttpExceptionFilter, TransformInterceptor } from './common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: { origin: ['http://localhost:3000', 'https://templehs.vercel.app'] } });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor())
